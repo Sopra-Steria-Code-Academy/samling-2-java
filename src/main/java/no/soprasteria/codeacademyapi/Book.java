963e134ab5eef9046c06ab18c3da4ad2caefd0d5
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2024-06-12 17:52:13 +0200

    Til Thomas. Hilsen Magnus

---
+++ src/main/java/no/soprasteria/codeacademyapi/Book.java
@@ -0,0 +1,10 @@
+package no.soprasteria.codeacademyapi;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+}
--- src/main/java/no/soprasteria/codeacademyapi/CodeacademyapiApplication.java
+++ src/main/java/no/soprasteria/codeacademyapi/CodeacademyapiApplication.java
@@ -3,20 +3,27 @@ package no.soprasteria.codeacademyapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(value = "/hello")
+@RequestMapping(value = "/v1/hello")
 public class CodeacademyapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodeacademyapiApplication.class, args);
     }
+
     @RequestMapping(value = "/world", method = RequestMethod.GET, produces = "text/plain")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello code Academy!");
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<Book> hello(@RequestBody Book book) {
+        return ResponseEntity.ok(book);
     }
 }
