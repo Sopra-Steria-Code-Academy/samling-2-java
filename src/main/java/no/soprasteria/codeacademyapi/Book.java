2732cc100f041c78cd30ed18e5af81dd009d6027
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2024-06-12 18:28:23 +0200

    Starten på oipenapi

--- src/main/java/no/soprasteria/codeacademyapi/Book.java
+++ src/main/java/no/soprasteria/codeacademyapi/Book.java
@@ -1,10 +1,14 @@
 package no.soprasteria.codeacademyapi;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class Book {
+    @Schema(example = "Maos lille røde", description = "Her kan man oppgi en tittel")
     private String title;
+    @Schema(example = "Mao")
     private String author;
+    @Schema(example = "2024")
     private int year;
 }
--- src/main/java/no/soprasteria/codeacademyapi/CodeacademyapiApplication.java
+++ src/main/java/no/soprasteria/codeacademyapi/CodeacademyapiApplication.java
@@ -1,5 +1,9 @@
 package no.soprasteria.codeacademyapi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.websocket.server.PathParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -22,6 +26,14 @@ public class CodeacademyapiApplication {
         return ResponseEntity.ok("Hello code Academy!");
     }
 
+    @Operation(summary = "Create a book")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Book created",
+                    content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "401", description = "You have no power here",
+                    content = @Content(mediaType = "text/plain")),
+            @ApiResponse(responseCode = "500", description = "My server has crashed. Sorry bro.",
+                    content = @Content(mediaType = "text/plain"))})
     @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Book> hello(@RequestBody Book book) {
         return ResponseEntity.ok(book);
